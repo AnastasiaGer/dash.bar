d05e670fb3338d44c012d601ed551338c522ec30
anisa <aagermanova@gmail.com>
2022-06-29 19:37:31 -0700

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS8sXzMTiRSO3JvdYLkZ1n77JHrstOZ38",
+  authDomain: "dash-bar-bd950.firebaseapp.com",
+  projectId: "dash-bar-bd950",
+  storageBucket: "dash-bar-bd950.appspot.com",
+  messagingSenderId: "318544697185",
+  appId: "1:318544697185:web:571be9a39ccb9ff112d720"
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+//init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export {projectFirestore, projectAuth, timestamp}
